c2f11c07fb0674bc58ec405b36e5bd4850802132
aurelwidyanti <aurelwyt@gmail.com>
2024-10-11 20:05:01 +0700

    refactor: Remove unused import and update routes in routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'views/home_screen.dart';
-import 'views/car_detail_screen.dart';
-import 'views/booking_screen.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
   '/': (context) => HomeScreen(),
