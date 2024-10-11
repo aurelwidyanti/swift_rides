2bf58980201b7338a532bf43d611493f374d6cb2
aurelwidyanti <aurelwyt@gmail.com>
2024-10-11 20:04:44 +0700

    refactor: Remove unused import and fix constructor parameter in MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'views/home_screen.dart';
 import 'routes.dart';
 
 void main() {
@@ -7,6 +6,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
