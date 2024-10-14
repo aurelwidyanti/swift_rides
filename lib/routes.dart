7f0de5639b0792794a727389e70a22edb5df9881
aurelwidyanti <aurelwyt@gmail.com>
2024-10-14 13:15:21 +0700

    refactor: Add CarDetailScreen route

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:swift_rides/views/car_detail_screen.dart';
 import 'views/home_screen.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
   '/': (context) => HomeScreen(),
+  '/car-detail': (context) => CarDetailScreen(),
 };
