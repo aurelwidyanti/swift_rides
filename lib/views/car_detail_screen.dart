7baba420328ec5350318a18218c0d4317545bf9e
aurelwidyanti <aurelwyt@gmail.com>
2024-10-14 13:14:58 +0700

    feat: Add CarDetailScreen to display car details

--- lib/views/car_detail_screen.dart
+++ lib/views/car_detail_screen.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+class CarDetailScreen extends StatelessWidget {
+  const CarDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Car Detail'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: const Center(
+        child: Text('Car Detail Screen'),
+      ),
+    );
+  }
+}
