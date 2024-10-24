b66e73f94b2c59cce467712c00270e38fcbe7878
aurelwidyanti <aurelwyt@gmail.com>
2024-10-24 16:48:39 +0700

    refactor: Update main.dart and routes.dart
    - Update main.dart to import necessary dependencies and use the Entrypoint widget as the home screen.
    - Remove routes.dart as it is no longer needed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
-import 'routes.dart';
+import 'package:swift_rides/views/history/history_screen.dart';
+import 'package:swift_rides/views/home/home_screen.dart';
+import 'package:swift_rides/views/profile/profile_screen.dart';
+import 'package:swift_rides/widgets/custom_bottom_bar.dart';
+import 'utils/entrypoint.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Car Rental App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/',
-      routes: appRoutes,
-    );
-  }
+Future<void> main() async {
+  runApp(MaterialApp(
+    title: 'Swift Rides',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const Entrypoint(),
+  ));
 }
