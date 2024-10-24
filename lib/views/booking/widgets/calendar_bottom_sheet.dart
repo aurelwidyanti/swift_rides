6cfbb2f4665778747b8ca277e865da9db1273965
aurelwidyanti <aurelwyt@gmail.com>
2024-10-24 16:58:38 +0700

    feat: Add CalendarBottomSheet widget

---
+++ lib/views/booking/widgets/calendar_bottom_sheet.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+class CalendarBottomSheet extends StatefulWidget {
+  const CalendarBottomSheet({super.key});
+
+  @override
+  State<CalendarBottomSheet> createState() => _CalendarBottomSheetState();
+}
+
+class _CalendarBottomSheetState extends State<CalendarBottomSheet> {
+  DateRangePickerController _datePickerController = DateRangePickerController();
+  DateTime? startDate;
+  DateTime? endDate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(24, 24, 24, 24),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SfDateRangePicker(
+            headerStyle: const DateRangePickerHeaderStyle(
+              backgroundColor: Colors.white,
+              textAlign: TextAlign.center,
+              textStyle: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            todayHighlightColor: const Color.fromRGBO(23, 93, 227, 1),
+            selectionColor: const Color.fromRGBO(23, 93, 227, 1),
+            startRangeSelectionColor: const Color.fromRGBO(23, 93, 227, 1),
+            endRangeSelectionColor: const Color.fromRGBO(23, 93, 227, 1),
+            rangeSelectionColor: const Color.fromRGBO(23, 93, 227, 0.2),
+            backgroundColor: Colors.white,
+            controller: _datePickerController,
+            selectionMode: DateRangePickerSelectionMode.range,
+            onSelectionChanged: (DateRangePickerSelectionChangedArgs args) {
+              setState(() {
+                if (args.value is PickerDateRange) {
+                  startDate = args.value.startDate;
+                  endDate = args.value.endDate ?? args.value.startDate;
+                }
+              });
+            },
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                if (startDate != null && endDate != null) {
+                  Navigator.pop(
+                      context, {'startDate': startDate, 'endDate': endDate});
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromRGBO(23, 93, 227, 1),
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text("Next"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
