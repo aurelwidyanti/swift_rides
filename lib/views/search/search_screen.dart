26813fe7eb66f86fb6f9c5508d4ca8ade583ac08
aurelwidyanti <aurelwyt@gmail.com>
2024-10-27 22:29:10 +0700

    refactor: Update SearchScreen with search bar and filter chips

--- lib/views/search/search_screen.dart
+++ lib/views/search/search_screen.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
+import 'package:swift_rides/views/search/widgets/search_filter.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(24, 64, 24, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Search...',
+                prefixIcon: const HugeIcon(
+                  icon: HugeIcons.strokeRoundedSearch01,
+                  color: Colors.black,
+                  size: 22,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              onChanged: (value) {},
+            ),
+            const SizedBox(height: 24),
+            const Text(
+              "Search by brand",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 4),
+            const SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SearchFilter(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
