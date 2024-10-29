c45d4d3ae3991bf8938b6231af6d39e2a80f90fb
aurelwidyanti <aurelwyt@gmail.com>
2024-10-29 18:19:20 +0700

    refactor: Update HomeScreen with padding and alignment changes

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -13,30 +13,36 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
+      body: SafeArea(
         child: Container(
+          padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+          alignment: Alignment.center,
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.topRight,
               end: Alignment.bottomLeft,
+              stops: [
+                0.1,
+                0.4,
+              ],
               colors: [
                 Color.fromRGBO(175, 203, 255, 1),
                 Color.fromRGBO(255, 255, 255, 1)
               ],
             ),
           ),
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(16, 64, 16, 0),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Row(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Row(
                     children: [
                       HugeIcon(
-                        icon: HugeIcons.strokeRoundedLocation04,
+                        icon: HugeIcons.strokeRoundedLocation01,
                         color: Colors.black,
-                        size: 28,
+                        size: 24.0,
                       ),
                       SizedBox(width: 12),
                       Column(
@@ -61,87 +67,95 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ],
                   ),
-                  const SizedBox(height: 24),
-                  const HomeBanner(),
-                  const SizedBox(height: 40),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Recommendation',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 24),
+                const HomeBanner(),
+                const SizedBox(height: 40),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Recommendation',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 16),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          CarCard(
+                            rating: 4.9,
+                            name: 'BMW',
+                            type: 'M4 Competition',
+                            imageUrl: 'assets/img/1.png',
+                            price: 800,
+                            fuel: 'Petrol',
+                            seat: 2,
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const CarDetailScreen(
+                                    name: 'BMW',
+                                    type: 'M4 Competition M xDrive',
+                                    price: 800,
+                                    imageUrl: 'assets/img/1.png',
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                          CarCard(
+                            rating: 4.8,
+                            name: 'BMW',
+                            type: 'M2 Coupé',
+                            imageUrl: 'assets/img/3.png',
+                            price: 700,
+                            fuel: 'Petrol',
+                            seat: 2,
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const CarDetailScreen(
+                                    name: 'BMW',
+                                    type: 'M2 Coupé',
+                                    price: 700,
+                                    imageUrl: 'assets/img/3.png',
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                          CarCard(
+                            rating: 4.7,
+                            name: 'BMW',
+                            type: 'M4 Convertible',
+                            imageUrl: 'assets/img/2.png',
+                            price: 800,
+                            fuel: 'Petrol',
+                            seat: 2,
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const CarDetailScreen(
+                                    name: 'BMW',
+                                    type: 'M4 Convertible',
+                                    price: 800,
+                                    imageUrl: 'assets/img/2.png',
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ],
                       ),
-                      CarCard(
-                        rating: 4.9,
-                        name: 'BMW',
-                        type: 'M4 Competition M xDrive',
-                        imageUrl: 'assets/img/1.png',
-                        price: 800,
-                        fuel: 'Petrol',
-                        seat: 2,
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const CarDetailScreen(
-                                name: 'BMW',
-                                type: 'M4 Competition M xDrive',
-                                price: 800,
-                                imageUrl: 'assets/img/1.png',
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                      CarCard(
-                        rating: 4.8,
-                        name: 'BMW',
-                        type: 'M2 Coupé',
-                        imageUrl: 'assets/img/3.png',
-                        price: 700,
-                        fuel: 'Petrol',
-                        seat: 2,
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const CarDetailScreen(
-                                name: 'BMW',
-                                type: 'M2 Coupé',
-                                price: 700,
-                                imageUrl: 'assets/img/3.png',
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                      CarCard(
-                        rating: 4.7,
-                        name: 'BMW',
-                        type: 'M4 Convertible',
-                        imageUrl: 'assets/img/2.png',
-                        price: 800,
-                        fuel: 'Petrol',
-                        seat: 2,
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const CarDetailScreen(
-                                name: 'BMW',
-                                type: 'M4 Convertible',
-                                price: 800,
-                                imageUrl: 'assets/img/2.png',
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
