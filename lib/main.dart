abca4f4f2e7f321f0a5cc0828e0ce313cdb0c728
aurelwidyanti <aurelwyt@gmail.com>
2024-10-29 14:53:00 +0700

    refactor: Remove debug banner in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'utils/entrypoint.dart';
 Future<void> main() async {
   runApp(MaterialApp(
     title: 'Swift Rides',
+    debugShowCheckedModeBanner: false,
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
