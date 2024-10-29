63503739c018d4677ed021c1cc9c6d3172faa339
aurelwidyanti <aurelwyt@gmail.com>
2024-10-29 22:40:02 +0700

    refactor: Remove unused import and entrypoint.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:swift_rides/views/splash_screen.dart';
 
-import 'utils/entrypoint.dart';
-
 Future<void> main() async {
   runApp(MaterialApp(
     title: 'Swift Rides',
