ceb1d8c39ffc4066ce7d5f5707a7ca48948c291b
aurelwidyanti <aurelwyt@gmail.com>
2024-10-30 13:00:31 +0700

    refactor: Adjust the layout and styling of the FilterChips for better visual appeal

--- lib/views/search/widgets/search_filter.dart
+++ lib/views/search/widgets/search_filter.dart
@@ -5,24 +5,60 @@ class SearchFilter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<String> carBrands = [
-      'BMW',
-      'Audi',
-      'Mercedes',
-      'Tesla',
-      'Rolls-Royce'
+    final List<Map<String, String>> carCategories = [
+      {'label': 'Cabriolet', 'image': 'assets/img/car-types/cabriolet.png'},
+      {'label': 'Coupe', 'image': 'assets/img/car-types/coupe.png'},
+      {'label': 'SUV', 'image': 'assets/img/car-types/cuv.png'},
+      {'label': 'Hatchback', 'image': 'assets/img/car-types/hatchback.png'},
+      {'label': 'Micro', 'image': 'assets/img/car-types/micro.png'},
+      {'label': 'Pickup', 'image': 'assets/img/car-types/pickup.png'},
+      {'label': 'Sedan', 'image': 'assets/img/car-types/sedan.png'},
+      {'label': 'Supercar', 'image': 'assets/img/car-types/supercar.png'},
     ];
 
+    final uniqueCategories = carCategories.toSet().toList();
+
     return Wrap(
-      spacing: 8.0,
-      alignment: WrapAlignment.start,
-      children: carBrands.map((brand) {
-        return FilterChip(
-          label: Text(brand),
-          onSelected: (bool value) {},
-          backgroundColor: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
+      spacing: 8,
+      runSpacing: 8,
+      children: uniqueCategories.map((category) {
+        return SizedBox(
+          width: MediaQuery.of(context).size.width * 0.44,
+          height: 150,
+          child: InkWell(
+            onTap: () {},
+            child: Card(
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+                side: const BorderSide(
+                  color: Color.fromARGB(255, 220, 220, 220),
+                  width: 1,
+                ),
+              ),
+              elevation: 1,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(24, 16, 24, 16),
+                child: Column(
+                  children: [
+                    Image.asset(
+                      category['image']!,
+                      width: double.infinity,
+                      height: 80,
+                      fit: BoxFit.fitWidth,
+                    ),
+                    Text(
+                      category['label']!,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         );
       }).toList(),
