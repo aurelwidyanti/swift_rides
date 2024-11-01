9e7ef344ba04fcb33a2417fb0fab2bb261dee590
aurelwidyanti <aurelwyt@gmail.com>
2024-11-01 18:18:54 +0700

    refactor: Enhance HomeBanner widget with customizable image and improved styling

--- lib/views/home/widget/home_banner.dart
+++ lib/views/home/widget/home_banner.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
 class HomeBanner extends StatelessWidget {
-  const HomeBanner({Key? key}) : super(key: key);
+  final String image;
+
+  const HomeBanner({Key? key, required this.image}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      child: Image.asset(
-        'assets/img/banner.png',
-        fit: BoxFit.cover,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(24),
+        ),
+        image: DecorationImage(
+          image: AssetImage(image),
+          fit: BoxFit.cover,
+        ),
+        border: Border.all(
+          color: const Color.fromARGB(255, 220, 220, 220),
+          width: 2,
+        ),
       ),
     );
   }
