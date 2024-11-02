89c9e8b1f31d16d1ed3fba7971ebc183a77ac55e
aurelwidyanti <aurelwyt@gmail.com>
2024-11-02 12:48:04 +0700

    refactor: Remove unused imports from home_screen and custom_text_field

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:swift_rides/utils/constants.dart';
 import 'package:swift_rides/views/home/car_detail_screen.dart';
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hugeicons/hugeicons.dart';
 
 class CustomTextField extends StatelessWidget {
   final String hintText;
