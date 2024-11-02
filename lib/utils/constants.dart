bf344d861859852b13144ba4d7dd3d5c978feb0b
aurelwidyanti <aurelwyt@gmail.com>
2024-11-02 14:18:55 +0700

    refactor: Add time and status filter constants to improve filtering options

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,3 +3,16 @@ List<String> demoBanner = [
   'assets/img/banner/banner2.png',
   'assets/img/banner/banner3.png',
 ];
+
+List<String> timeFilter = [
+  'Semua',
+  'Har ini',
+  'Minggu ini',
+  'Pilih Bulan',
+];
+
+List<String> statusFilter = [
+  'Semua',
+  'Gagal',
+  'Sukses',
+];
