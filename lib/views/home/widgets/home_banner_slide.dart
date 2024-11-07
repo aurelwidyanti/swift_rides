4af1660bdf3947ff96c51d6d0a573491191f3bdd
aurelwidyanti <aurelwyt@gmail.com>
2024-11-07 14:25:22 +0700

    feat: Add HomeBannerSlide widget for image sliding functionality with page indicator

---
+++ lib/views/home/widgets/home_banner_slide.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:swift_rides/views/home/widgets/home_banner.dart';
+import 'package:swift_rides/widgets/custom_page_indicator.dart';
+
+class HomeBannerSlide extends StatefulWidget {
+  const HomeBannerSlide({
+    super.key,
+    required this.images,
+  });
+
+  final List images;
+
+  @override
+  State<HomeBannerSlide> createState() => _HomeBannerSlideState();
+}
+
+class _HomeBannerSlideState extends State<HomeBannerSlide> {
+  int intialIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.81,
+      child: Stack(
+        children: [
+          PageView.builder(
+            onPageChanged: (value) {
+              setState(() {
+                intialIndex = value;
+              });
+            },
+            itemCount: widget.images.length,
+            itemBuilder: (context, index) =>
+                HomeBanner(image: widget.images[index]),
+          ),
+          Positioned(
+            bottom: 16,
+            left: 0,
+            right: 0,
+            child: CustomPageIndicator(
+              pageLength: widget.images.length,
+              currentIndexPage: intialIndex,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
