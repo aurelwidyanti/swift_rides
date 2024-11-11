ab321f852dbccba7f31618a7b9491cad6688765b
aurelwidyanti <aurelwyt@gmail.com>
2024-11-11 20:24:12 +0700

    refactor: Add labelText property and update AuthTextField layout for improved usability

--- lib/views/auth/widgets/auth_text_field.dart
+++ lib/views/auth/widgets/auth_text_field.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 class AuthTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
+  final String labelText;
   final bool obscureText;
   final TextInputAction textInputAction;
   final TextInputType keyboardType;
@@ -14,6 +15,7 @@ class AuthTextField extends StatelessWidget {
     Key? key,
     required this.controller,
     required this.hintText,
+    required this.labelText,
     this.obscureText = false,
     this.textInputAction = TextInputAction.done,
     this.keyboardType = TextInputType.text,
@@ -23,45 +25,59 @@ class AuthTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-      height: 44,
-      width: 360,
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 237, 237, 237),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: TextFormField(
-        controller: controller,
-        obscureText: obscureText,
-        textInputAction: textInputAction,
-        keyboardType: keyboardType,
-        validator: validator,
-        decoration: InputDecoration(
-            hintText: hintText,
-            hintStyle: const TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.normal,
-              fontSize: 15,
-            ),
-            suffixIcon: onSuffixIconTap != null
-                ? GestureDetector(
-                    onTap: onSuffixIconTap,
-                    child: obscureText
-                        ? const Icon(
-                            Icons.visibility_off,
-                            color: Colors.grey,
-                            size: 22,
-                          )
-                        : const Icon(
-                            Icons.visibility,
-                            color: Colors.grey,
-                            size: 22,
-                          ),
-                  )
-                : null,
-            border: InputBorder.none),
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          labelText,
+          style: const TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w500,
+            fontSize: 15,
+          ),
+        ),
+        const SizedBox(height: 8),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          height: 44,
+          width: 360,
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 237, 237, 237),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: TextFormField(
+            controller: controller,
+            obscureText: obscureText,
+            textInputAction: textInputAction,
+            keyboardType: keyboardType,
+            validator: validator,
+            decoration: InputDecoration(
+                hintText: hintText,
+                hintStyle: const TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 15,
+                ),
+                suffixIcon: onSuffixIconTap != null
+                    ? GestureDetector(
+                        onTap: onSuffixIconTap,
+                        child: obscureText
+                            ? const Icon(
+                                Icons.visibility_off,
+                                color: Colors.grey,
+                                size: 22,
+                              )
+                            : const Icon(
+                                Icons.visibility,
+                                color: Colors.grey,
+                                size: 22,
+                              ),
+                      )
+                    : null,
+                border: InputBorder.none),
+          ),
+        ),
+      ],
     );
   }
 }
