34af733d41cec294fc4450d40d78ac47326295b9
aurelwidyanti <aurelwyt@gmail.com>
2024-11-22 17:54:41 +0700

    refactor: Adjust font sizes and padding in HomeScreen for improved layout

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -39,14 +39,14 @@ class HomeScreen extends StatelessWidget {
                           const Text(
                             'Welcome back,',
                             style: TextStyle(
-                              fontSize: 22,
+                              fontSize: 20,
                               fontWeight: FontWeight.normal,
                             ),
                           ),
                           Text(
                             user?.name ?? 'User',
                             style: const TextStyle(
-                              fontSize: 22,
+                              fontSize: 20,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
@@ -77,13 +77,13 @@ class HomeScreen extends StatelessWidget {
                     ],
                   ),
                 ),
-                const SizedBox(height: 16),
+                const SizedBox(height: 8),
                 Container(
                   padding: const EdgeInsets.all(16),
                   child: HomeBannerSlide(images: demoBanner),
                 ),
                 Container(
-                  padding: const EdgeInsets.fromLTRB(16, 24, 0, 24),
+                  padding: const EdgeInsets.fromLTRB(16, 8, 0, 16),
                   child: const Column(
                     children: [
                       Column(
@@ -92,7 +92,7 @@ class HomeScreen extends StatelessWidget {
                           Text(
                             'Recommendation',
                             style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
+                                fontSize: 18, fontWeight: FontWeight.bold),
                           ),
                           SizedBox(height: 8),
                           CarSlider(),
@@ -105,7 +105,7 @@ class HomeScreen extends StatelessWidget {
                           Text(
                             'New Cars',
                             style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
+                                fontSize: 18, fontWeight: FontWeight.bold),
                           ),
                           SizedBox(height: 8),
                           CarSlider(),
